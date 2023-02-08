f7d1a4a880ea0588e7cc913f33458c48ef328810
Deepak Pathak <pathakdeepak024@gmail.com>
2023-02-08 18:44:39 +0530

    MinMaxInArray

---
+++ src/main/java/Arrays/MinMaxInArray.java
@@ -0,0 +1,71 @@
+package Arrays;
+
+/**
+ * @author valar_morghulis on 08/02/23.
+ * @project Coding-Interview
+ * @description
+ */
+public class MinMaxInArray {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{22, 14, 8, 17, 35, 3};
+        Values values = new Values();
+        minMax(arr,values);
+        System.out.println("min value: " + values.getMin());
+        System.out.println("max value: " + values.getMax());
+    }
+
+    static void minMax (int[] arr, Values minMaxValues) {
+        if (arr.length == 1){
+            minMaxValues.setMin(arr[0]);
+            minMaxValues.setMax(arr[0]);
+        }
+        if (arr.length == 2){
+            if (arr[0] < arr[1])
+            {
+                minMaxValues.setMin(arr[0]);
+                minMaxValues.setMax(arr[1]);
+            }
+            else{
+                minMaxValues.setMin(arr[1]);
+                minMaxValues.setMax(arr[0]);
+            }
+        }
+        if (arr.length > 2){
+            int min = arr[0];
+            int max = arr[1];
+            for (int i = 2; i < arr.length; i++){
+                if (arr[i] < min)
+                    min = arr[i];
+                else if (arr[i] > max)
+                    max = arr[i];
+            }
+            minMaxValues.setMin(min);
+            minMaxValues.setMax(max);
+        }
+
+    }
+
+
+
+    static class Values {
+        int min;
+        int max;
+
+        public int getMin() {
+            return min;
+        }
+
+        public void setMin(int min) {
+            this.min = min;
+        }
+
+        public int getMax() {
+            return max;
+        }
+
+        public void setMax(int max) {
+            this.max = max;
+        }
+    }
+}
